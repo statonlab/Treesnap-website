af2ad57dcf33b9af87a6caa52c35b1f911ab222c
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-03-22 13:29:04 -0400

    Add web api observations controller and add is_private column to observations table. Also make sure private observations don't get returned

--- app/Http/Controllers/ObservationsController.php
+++ app/Http/Controllers/ObservationsController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Controllers\Api\v1\ResponseTrait;
 use App\Observation;
 use Illuminate\Http\Request;
 
 class ObservationsController extends Controller
 {
+    use ResponseTrait;
+
     public function index()
     {
         $observations = Observation::all();
         $data = [];
 
         foreach ($observations as $observation) {
+            // Skip private observations
+            if ($observation->is_private) {
+                continue;
+            }
+
+            // Compile the data into a standardized response
             $data[] = [
               'id' => $observation->id,
               'observation_category' => $observation->observation_category,
@@ -27,8 +36,6 @@ class ObservationsController extends Controller
             ];
         }
 
-        return [
-          'data' => $data,
-        ];
+        return $this->success($data);
     }
 }
