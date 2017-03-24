251bbbe354f710a24f0c8e653c188de2da6799d5
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-03-24 12:07:06 -0400

    few fixes

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,6 @@ Route::group([
   'namespace' => 'Api\v1',
   'middleware' => ['auth:api'],
 ], function () {
-
     // Observations Controller
     Route::get('/observations', 'ObservationsController@index');
     Route::post('/observations', 'ObservationsController@create');
