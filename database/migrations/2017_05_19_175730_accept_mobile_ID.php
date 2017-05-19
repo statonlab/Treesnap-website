6e8f965adcaa0bb07f27e7ca45f69615eed0a4c4
Bradford Condon <bradford.condon@gmail.com>
2017-05-19 14:44:11 -0400

    allow private user

---
+++ database/migrations/2017_05_19_175730_accept_mobile_ID.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AcceptMobileID extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('observations', function (Blueprint $table) {
+
+            $table->integer('mobile_id');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('observations', function (Blueprint $table) {
+            $table->dropColumn('mobile_d');
+
+        });
+    }
+}
