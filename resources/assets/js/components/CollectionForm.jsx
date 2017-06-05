6b2715e09d7b0c825c79ebe439f0e1bccb97331f
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-06-05 13:02:18 -0400

    Create flags controller and add collections functionality

--- resources/assets/js/components/CollectionForm.jsx
+++ resources/assets/js/components/CollectionForm.jsx
@@ -1,18 +1,73 @@
 import React, {Component} from 'react'
 import PropTypes from 'prop-types'
+import Spinner from './Spinner'
 
 export default class CollectionForm extends Component {
     constructor(props) {
         super(props)
 
         this.state = {
-            collectionId: 0,
-            name        : ''
+            loading      : false,
+            collection_id: 0,
+            label        : '',
+            errors       : {
+                label        : [],
+                collection_id: []
+            }
         }
     }
 
     submit(event) {
         event.preventDefault()
+
+        if (!this.validate()) {
+            return
+        }
+
+        this.setState({loading: true})
+
+        axios.post('/collection/attach', {
+            observation_id: this.props.observationId,
+            collection_id : this.state.collection_id || '',
+            label         : this.state.label
+        }).then(response => {
+            this.setState({loading: true})
+            this.props.onSubmit(response.data.data)
+        }).catch(error => {
+            const response = error.response
+            if (response && response.status === 422) {
+                const data = response.data
+                this.setState({
+                    loading: false,
+                    errors : {
+                        label        : data.label || [],
+                        collection_id: data.collection_id || []
+                    }
+                })
+
+                return
+            }
+
+            console.log(error)
+        })
+    }
+
+    validate() {
+        let label         = this.state.label
+        let collection_id = this.state.collection_id
+
+        if (label.trim() === '' && collection_id === 0) {
+            this.setState({
+                errors: {
+                    label        : ['Please select an existing collection or type a new collection label.'],
+                    collection_id: []
+                }
+            })
+
+            return false
+        }
+
+        return true
     }
 
     render() {
@@ -21,14 +76,18 @@ export default class CollectionForm extends Component {
                 <div className="field">
                     <label className="label">Collection Name</label>
                     <div className="control">
-                        <span className="select">
-                            <select value={this.state.collectionId} onChange={({target}) => this.setState({collectionId: target.value})}>
+                        <span className={`select${this.state.errors.collection_id.length > 0 ? ' is-danger' : ''}`}>
+                            <select value={this.state.collection_id}
+                                    onChange={({target}) => this.setState({collection_id: target.value})}>
                                 <option value="0">[Select Collection]</option>
                                 {this.props.collections.map((collection, index) => {
                                     return (<option value={collection.value} key={index}>{collection.label}</option>)
                                 })}
                             </select>
                         </span>
+                        {this.state.errors.collection_id.map((error, index) => {
+                            return (<p className="help is-danger" key={index}>{error}</p>)
+                        })}
                     </div>
                 </div>
 
@@ -36,11 +95,14 @@ export default class CollectionForm extends Component {
                     <label className="label">Or Create New Collection</label>
                     <div className="control">
                         <input type="text"
-                               className="input"
-                               onChange={({target}) => this.setState({name: target.value})}
-                               value={this.state.name}
-                               placeholder="Type new collection name"
+                               className={`input${this.state.errors.label.length > 0 ? ' is-danger' : ''}`}
+                               onChange={({target}) => this.setState({label: target.value})}
+                               value={this.state.label}
+                               placeholder="Type new collection label"
                         />
+                        {this.state.errors.label.map((error, index) => {
+                            return (<p className="help is-danger" key={index}>{error}</p>)
+                        })}
                     </div>
                     <p className="help">
                         The selected observation will be automatically added to the newly created collection.
@@ -52,6 +114,8 @@ export default class CollectionForm extends Component {
                         <button type="submit" className="button is-primary">Add Observation</button>
                     </div>
                 </div>
+
+                <Spinner visible={this.state.loading}/>
             </form>
         )
     }
@@ -59,9 +123,12 @@ export default class CollectionForm extends Component {
 
 CollectionForm.PropTypes = {
     observationId: PropTypes.number.isRequired,
-    collections  : PropTypes.array
+    collections  : PropTypes.array,
+    onSubmit     : PropTypes.func
 }
 
 CollectionForm.defaultProps = {
-    collections: []
+    collections: [],
+    onSubmit() {
+    }
 }
