c93bf20185cdf9e453e92cfd55cc009b1bb6c31a
Abdullah Almsaeed <almasaeed2010@gmail.com>
2017-06-06 13:43:27 -0400

    Add mailgun service

--- config/services.php
+++ config/services.php
@@ -15,7 +15,7 @@ return [
     */
 
     'mailgun' => [
-        'domain' => env('MAILGUN_DOMAIN'),
+        'domain' => env('MAILGUN_DOMAIN', 'email.treesnap.org'),
         'secret' => env('MAILGUN_SECRET'),
     ],
 
