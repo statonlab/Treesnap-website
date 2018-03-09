aea9cc35d62a5aa82e55925269b429999d8b7a7f
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-03-08 19:42:26 -0500

    Add Register page test

---
+++ tests/Browser/RegisterPageTest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Tests\Browser;
+
+use Carbon\Carbon;
+use Tests\Browser\Pages\RegisterPage;
+use Tests\DuskTestCase;
+use Laravel\Dusk\Browser;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+
+class RegisterPageTest extends DuskTestCase
+{
+    /**
+     * A Dusk test example.
+     *
+     * @return void
+     * @throws
+     */
+    public function testExample()
+    {
+        $this->browse(function (Browser $browser) {
+            $browser->visit(new RegisterPage())
+                ->assertSee('Register')
+                ->type('name', 'Test User')
+                ->type('email', 'myemail@example.com')
+                ->type('zipcode', '12345')
+                ->type('password', 'secretpass')
+                ->type('password_confirmation', 'secretpass')
+                ->select('birth_year', Carbon::now()->year - 2)
+                ->waitForText('guardian consent')
+                ->check('minorConsent')
+                ->check('agreement')
+                ->press('Register')
+                ->assertPathIs('/');
+        });
+    }
+}
