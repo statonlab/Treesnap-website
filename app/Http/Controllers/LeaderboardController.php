a88fe275b080299c2a0a4f83466b0b45f3191fee
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-04-16 16:02:15 -0400

    Remove admins

--- app/Http/Controllers/LeaderboardController.php
+++ app/Http/Controllers/LeaderboardController.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Traits\Responds;
 use App\Observation;
+use App\Role;
 use Carbon\Carbon;
 use DB;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 class LeaderboardController extends Controller
@@ -25,6 +27,8 @@ class LeaderboardController extends Controller
         /** @var \App\User $user */
         $user = $request->user();
 
+        $admin_role_id = Role::where('is_admin', true)->first()->id;
+
         // Get observations grouped by user sorted by date and limited the last 30 days
         // If null or less than $limit is available, get the next 30 days until $limit is reached
         $leaders = [];
@@ -32,19 +36,16 @@ class LeaderboardController extends Controller
         $observations = Observation::select([
             'user_id',
             DB::raw('COUNT(*) as observations_count'),
-        ])
-            ->with([
+        ])->whereHas('user', function ($query) use ($admin_role_id) {
+            $query->where('role_id', '!=', $admin_role_id);
+        })->with([
                 'user' => function ($query) {
                     $query->select(['id', 'name', 'is_anonymous']);
                 },
-            ])
-            ->when($limit > 0, function ($query) use ($limit) {
+            ])->when($limit > 0, function ($query) use ($limit) {
                 $query->limit($limit);
-            })
-            //->where('created_at', '>', Carbon::now()->subDays(30))
-            ->groupBy('user_id')
-            ->orderBy('observations_count', 'desc')
-            ->get();
+            })//->where('created_at', '>', Carbon::now()->subDays(30))
+            ->groupBy('user_id')->orderBy('observations_count', 'desc')->get();
 
         $isAdmin = $user ? $user->isAdmin() || $user->isScientist() : false;
         foreach ($observations as $observation) {
