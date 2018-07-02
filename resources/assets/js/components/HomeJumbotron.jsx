d1eece2802a65a6690a6f294a7c93c85e3c548b1
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-07-02 07:24:38 -0400

    Fix 302

--- resources/assets/js/components/HomeJumbotron.jsx
+++ resources/assets/js/components/HomeJumbotron.jsx
@@ -1,5 +1,6 @@
 import React, {Component} from 'react'
 import GetAppRow from './GetAppRow'
+import {Link} from 'react-router-dom'
 
 export default class HomeJumbotron extends Component {
   render() {
@@ -11,7 +12,7 @@ export default class HomeJumbotron extends Component {
               <div className="home-text">
                 <h3 className="title is-4 mb-none">AVAILABLE NOW</h3>
                 <h1 className="title is-1">Help Our Nation’s Trees!</h1>
-                <p>
+                <p className={'mb-1'}>
                   Invasive diseases and pests threaten the health of America’s forests.
                   Scientists are working to understand what allows some individual trees
                   to survive, but they need to find healthy, resilient trees in the
@@ -20,6 +21,9 @@ export default class HomeJumbotron extends Component {
                   your property, or out in the wild using TreeSnap! Scientists will use
                   the data you collect to locate trees for research projects like studying
                   the genetic diversity of tree species and building better tree breeding programs.
+                </p>
+                <p>
+                  <Link to="/partners" className={'button is-borderless'}>Meet the scientists that use TreeSnap data</Link>
                 </p>
               </div>
 
