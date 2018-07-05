c7728a3d5d3003436c7bcfb5fa24ed83019db601
Abdullah Almsaeed <almasaeed2010@gmail.com>
2018-07-05 08:17:59 -0400

    Reduce number of seeded material

--- database/seeds/EventsTableSeeder.php
+++ database/seeds/EventsTableSeeder.php
@@ -11,6 +11,6 @@ class EventsTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(\App\Event::class, 10)->create();
+        factory(\App\Event::class, 2)->create();
     }
 }
