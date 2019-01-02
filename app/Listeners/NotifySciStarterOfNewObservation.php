6fba57755e91f4db11e54927e630c3d94d82c0ed
Abdullah Almsaeed <almasaeed2010@gmail.com>
2019-01-02 15:03:43 -0500

    Remove debug code

--- app/Listeners/NotifySciStarterOfNewObservation.php
+++ app/Listeners/NotifySciStarterOfNewObservation.php
@@ -25,29 +25,21 @@ class NotifySciStarterOfNewObservation implements ShouldQueue
      */
     public function handle($event)
     {
-        \Log::info('Triggered!!!!!');
-
         $observation = $event->observation;
         $sci_starter = new SciStarterResource();
         $profile_id = $sci_starter->profile($observation->user);
 
-        \Log::info('PROFILE ID: '.json_encode(['d' => $profile_id ? $profile_id : 'NONE! '.$observation->user->email], JSON_PRETTY_PRINT));
-
         if ($profile_id) {
             try {
-                $response = $sci_starter->event($profile_id, 'collection', [
+                $sci_starter->event($profile_id, 'collection', [
                     'when' => $observation->collection_date->format('Y-MM-DD\THH:mm:ss'),
                     'extra' => json_encode([
                         'title' => $observation->observation_category === 'Other' ? $observation->data['otherLabel'] : $observation->observation_category,
                     ]),
                 ]);
-                $message = json_encode($response, JSON_PRETTY_PRINT);
-                \Log::info($message);
             } catch (Exception $exception) {
                 \Log::error($exception->getMessage());
             }
-        } else{
-            \Log::info("id: $profile_id (could be empty). Next is truth: " . boolval($profile_id) ? 'TRUE' : 'FALSE');
         }
     }
 }
