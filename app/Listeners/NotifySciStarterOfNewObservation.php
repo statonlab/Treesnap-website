c113c2c02daaa8de39b199bf9685e02b5585a5c0
Abdullah Almsaeed <almasaeed2010@gmail.com>
2019-01-02 12:33:40 -0500

    Update debug code

--- app/Listeners/NotifySciStarterOfNewObservation.php
+++ app/Listeners/NotifySciStarterOfNewObservation.php
@@ -3,8 +3,6 @@
 namespace App\Listeners;
 
 use App\Http\Resources\SciStarterResource;
-use App\User;
-use Carbon\Carbon;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Exception;
@@ -12,16 +10,6 @@ use Exception;
 class NotifySciStarterOfNewObservation implements ShouldQueue
 {
     use InteractsWithQueue;
-
-    /**
-     * Create the event listener.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        //
-    }
 
     /**
      * Handle the event.
@@ -37,13 +25,14 @@ class NotifySciStarterOfNewObservation implements ShouldQueue
         $sci_starter = new SciStarterResource();
         $profile_id = $sci_starter->profile($observation->user);
         \Log::info('PROFILE ID: '.$profile_id);
-        if($profile_id) {
+
+        if ($profile_id) {
             try {
                 $response = $sci_starter->event($profile_id, 'collection', [
                     'when' => $observation->collection_date->format('Y-MM-DD\THH:mm:ss'),
                     'extra' => json_encode([
-                        'title' => $observation->observation_category === 'Other' ? $observation->data['otherLabel'] : $observation->observation_category
-                    ])
+                        'title' => $observation->observation_category === 'Other' ? $observation->data['otherLabel'] : $observation->observation_category,
+                    ]),
                 ]);
                 $message = json_encode($response, JSON_PRETTY_PRINT);
                 \Log::info($message);
