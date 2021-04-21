eb61f46f1322de06e9167c2501fa86fdbf6f7390
Abdullah Almsaeed <almasaeed2010@gmail.com>
2021-04-21 16:41:41 -0400

    Add url to downloadable

--- app/Http/Controllers/DownloadsController.php
+++ app/Http/Controllers/DownloadsController.php
@@ -253,7 +253,7 @@ class DownloadsController extends Controller
         ];
 
         // Add meta labels to header
-        return array_merge($header, $this->getMetaHeader()) + ['URL'];
+        return array_merge($header, $this->getMetaHeader(), ['Url']);
     }
 
     /**
@@ -303,8 +303,9 @@ class DownloadsController extends Controller
             "$observation->correct_marks times",
         ];
 
-        return array_merge($line,
-                $this->extractMetaData($observation)) + [url("observation/$observation->id")];
+        $url = [url("observation/$observation->id"];
+
+        return array_merge($line, $this->extractMetaData($observation), $url);
     }
 
     /**
