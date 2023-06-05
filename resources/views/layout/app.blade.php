34a34ca9d5ab47414b8458dde747420b9e535775
Noah Caldwell <noahgreycaldwell@gmail.com>
2023-06-05 10:54:40 -0500

    update ga to gua

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -12,24 +12,35 @@
         }
     </script>
     @yield('head')
-    <script>
-        (function (i, s, o, g, r, a, m) {
-            i['GoogleAnalyticsObject'] = r
-            i[r] = i[r] || function () {
-                    (i[r].q = i[r].q || []).push(arguments)
-                }, i[r].l = 1 * new Date()
-            a = s.createElement(o),
-                m = s.getElementsByTagName(o)[0]
-            a.async = 1
-            a.src   = g
-            m.parentNode.insertBefore(a, m)
-        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')
+{{--    <script>--}}
+{{--        (function (i, s, o, g, r, a, m) {--}}
+{{--            i['GoogleAnalyticsObject'] = r--}}
+{{--            i[r] = i[r] || function () {--}}
+{{--                    (i[r].q = i[r].q || []).push(arguments)--}}
+{{--                }, i[r].l = 1 * new Date()--}}
+{{--            a = s.createElement(o),--}}
+{{--                m = s.getElementsByTagName(o)[0]--}}
+{{--            a.async = 1--}}
+{{--            a.src   = g--}}
+{{--            m.parentNode.insertBefore(a, m)--}}
+{{--        })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga')--}}
 
-        ga('create', 'UA-102233923-1', 'auto')
-        ga('send', 'pageview')
+{{--        ga('create', 'UA-102233923-1', 'auto')--}}
+{{--        ga('send', 'pageview')--}}
 
-    </script>
+{{--    </script>--}}
 </head>
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-LFKXGPPDHV"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-LFKXGPPDHV');
+</script>
+
 <body>
 
 @include('layout.navbar')
