f917b3e88b8dab318a12491fbedd411dd83db6c3
Noah Caldwell <noahgreycaldwell@gmail.com>
2024-06-07 16:04:12 -0500

    small updates

--- app/Services/Analytics/ObservationStatistics.php
+++ app/Services/Analytics/ObservationStatistics.php
@@ -43,4 +43,46 @@ class ObservationStatistics
 
         return $indexed;
     }
+
+    use Http;
+
+//Http::withHeaders([
+//'X-Demo' => 'This is some header information',
+//])
+//->post('https://maps.googleapis.com/maps/api/geocode/json?latlng=40.714224,-73.961452&key=YOUR_API_KEY', [
+//'latlng' => 'Code Editor',
+//'key' => '6 stars only',
+//]);
+
+    public function aggregateObservationsByCountry()
+    {
+        $observations = Observation::select([
+            DB::raw('COUNT(*) as observations_count'),
+            'address',
+        ])
+            ->groupBy('address')
+            ->get();
+
+        $countries = [];
+
+        foreach ($observations as $observation) {
+            if (empty($observation->address)) {
+                continue;
+            }
+
+            foreach ($observation->address['components'] as $component) {
+                if (in_array('country', $component['types'])) {
+                    if (isset($countries[$component['long_name']])) {
+                        $countries[$component['long_name']] += $observation->observations_count;
+                    } else {
+                        $countries[$component['long_name']] = $observation->observations_count;
+                    }
+                }
+            }
+        }
+
+        arsort($countries);
+
+        return $countries;
+    }
 }
