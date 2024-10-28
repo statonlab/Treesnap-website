2a7ae2f34f326409dc372eb1d18107b59a8755ed
chance-stribling <cstribling1717@gmail.com>
2024-10-28 10:19:14 -0400

    if entry missing ashspeciesv2, add it to data

--- app/Console/Commands/AshSpeciesToV2.php
+++ app/Console/Commands/AshSpeciesToV2.php
@@ -27,4 +27,37 @@ class AshSpeciesToV2 extends Command
     {
         //
     }
+    public function ashSpeciesToAshSpeciesV2()
+    {
+        //for each observation
+        Observation::where("observation_category", 'Ash')
+            //200 observations at a time
+            ->chunk(200, function ($observations) {
+                
+                $ashSpeciesV2 = array(
+                    "ashSpeciesV2" => "Not Available",
+                );
+                
+                $i = 0;
+                foreach ($observations as $observation) {
+
+                    //...if ashSpecies isn't set, add that field to data
+                    if(!isset($observation->data['ashSpeciesV2'])){
+                        $observation->data = array_merge($observation->data, $ashSpeciesV2);
+                        $observation->save();
+                        $i++;
+                    } else{
+                        $this->info($observation->data['ashSpeciesV2']);
+                    }
+                }
+
+                if($i==1){
+                    $this->info($i.' entry has been updated.');
+                }
+                else{
+                    $this->info($i.' entries have been updated.');
+                }
+                
+            });
+    }
 }
