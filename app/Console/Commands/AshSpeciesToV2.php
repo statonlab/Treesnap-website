a929dff9da11d89db7e129338bf6b3fb9262cdee
chance-stribling <cstribling1717@gmail.com>
2024-10-28 09:31:46 -0400

    created empty command

---
+++ app/Console/Commands/AshSpeciesToV2.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class AshSpeciesToV2 extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:ash-species-to-v2';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+    }
+}
